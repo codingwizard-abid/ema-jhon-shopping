1aa8d1ddb8dcbdff9baeed740bd810653e575566
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-03-23 02:32:47 +0600

    calculation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = (props) => {
+const Cart = ({cart}) => {
+   let total = 0;
+   let shipping = 0;
+   for(let product of cart){
+      total += product.price;
+      shipping +=product.shipping;
+   }
+
+
+   let tax = total * 0.1;
    return (
-      <div>
+      <div className='cart'>
          <h2 style={{textAlign:'center'}}>Cart Summery</h2>
-         <p>Selected Items: {props.cart.length}</p>
+         <p>Selected Items: {cart.length}</p>
+         <p>Total Price: ${total}</p>
+         <p>Total Shipping: {shipping}</p>
+         <p>Tax: {tax.toFixed(2)}</p>
+         <h4>Grand Total: </h4>
       </div>
    );
 };
